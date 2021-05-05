a617b50b5a813b87357ba2491f925868fb38ae6e
lbp196 <tnfnflffk@gmail.com>
2021-05-05 22:23:27 +0900

    5.5 test

---
+++ 4.py
@@ -0,0 +1,20 @@
+
+# 불리언 3rd- 조건문 if else, elif, else
+a = 3
+b = 2
+c = 5
+d = c - a
+
+if a > b:        #a가 b보다 크면 Hello
+    print("Hello")
+if a > c:        #a가 c보다 크면 What?
+    print("What?")
+elif a == d:     #a가 3과 같다면 bye~!
+    print("Bye~!")
+else:            #a가 b보다 작으면 Hi
+    print("Hi~!")
+
+#함수 1st- 반복되는 코드들을 그룹으로 모아놓은 것을 말한다.
+
+print("철수: 안녕? 넌 몇 살이니?")      #라고 물어봐요.
+print("영희: 나? 나는 20살이야", "\n")  #라고 대답합니다.
